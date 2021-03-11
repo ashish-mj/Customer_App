b24b1bddd9b699d9886ca99f28b9349e0698e312
Ashish M J <ashm.jagadeesh@gmail.com>
2021-03-11 09:17:19 +0530

    Add CumtomerList

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,1 +1,21 @@
+import React, { Component } from 'react'
+import customers from './data';
+import Filter from './Filter';
+import CustomerRow from './CustomerRow';
 
+export default class CustomerList extends Component {
+    state={
+            "customers":customers
+    };
+    render() {
+        return (
+            <div>
+                <Filter/>
+                <h1> Customer List</h1>
+                {
+                    this.state.customers.customers.map(c=><CustomerRow customer={c} key={c.id}/>)
+                }
+            </div>
+        )
+    }
+}
