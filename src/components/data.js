739e70629ecccfd5c806a42acb5a236d70f17341
Ashish M J <ashm.jagadeesh@gmail.com>
2021-03-11 09:05:58 +0530

    Update files

---
+++ src/components/data.js
@@ -0,0 +1,20 @@
+const customers={
+    "customers":[
+        {"id":1,"Name":"Albin","Gender":"Male","Address":"Bangalore"},
+        {"id":2,"Name":"Charan","Gender":"Male","Address":"Bangalore"},
+        {"id":3,"Name":"Mahesh","Gender":"Male","Address":"Vizag"},
+        {"id":4,"Name":"Prasanth","Gender":"Male","Address":"Kolar"},
+        {"id":5,"Name":"Sambhabv","Gender":"Male","Address":"Indore"},
+        {"id":6,"Name":"Pritam","Gender":"Male","Address":"Ranchi"},
+        {"id":7,"Name":"Shubham","Gender":"Male","Address":"Jamshepur"},
+        {"id":8,"Name":"Ashish","Gender":"Male","Address":"Mysuru"},
+        {"id":9,"Name":"Avinash","Gender":"Male","Address":"Agra"},
+        {"id":10,"Name":"Darsan","Gender":"Male","Address":"Bangalore"},
+        {"id":11,"Name":"Venkat","Gender":"Male","Address":"Chikamagaluru"},
+        {"id":12,"Name":"Ananya","Gender":"Female","Address":"Chikamagaluru"},
+        {"id":13,"Name":"Dheeraj","Gender":"Male","Address":"Kolar"},
+        {"id":13,"Name":"Jaideep","Gender":"Male","Address":"Gulbarga"}
+
+    ]
+};
+export default customers;
